513740ad341f6d32246454c2e6458128f0485e50
Scott Coleman - wsl <scott@sjcdev.com>
2026-03-10 06:03:20 -0400

    US

--- examples/sta_connect/main/src/sta_connect.c
+++ examples/sta_connect/main/src/sta_connect.c
@@ -21,6 +21,7 @@
 #include "mm_app_regdb.h"
 
 // #define COUNTRY_CODE "AU"
+#define COUNTRY_CODE "US"
 #ifndef COUNTRY_CODE
 #error COUNTRY_CODE must be defined to the appropriate 2 character country code. \
        See mm_app_regdb.c for valid options.
